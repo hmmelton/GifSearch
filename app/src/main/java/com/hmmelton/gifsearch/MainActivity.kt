00301fd1c40a4c851aa2539578369c25037a91b8
Harrison Melton <harrisonmelton@Harrisons-MacBook-Pro.local>
2019-02-12 20:40:53 -0800

    added ktlint & removed example tests

--- app/src/main/java/com/hmmelton/gifsearch/MainActivity.kt
+++ app/src/main/java/com/hmmelton/gifsearch/MainActivity.kt
@@ -2,7 +2,7 @@ package com.hmmelton.gifsearch
 
 import android.os.Bundle
 import android.support.design.widget.Snackbar
-import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
 
