12d95cc5d83aa9b2b3c851d3be984835227db1c9
Harrison Melton <harrisonmelton@Harrisons-MacBook-Pro.local>
2019-02-12 21:41:35 -0800

    added documentation

--- app/src/main/java/com/hmmelton/gifsearch/network/GifService.kt
+++ app/src/main/java/com/hmmelton/gifsearch/network/GifService.kt
@@ -9,9 +9,26 @@ import retrofit2.http.Query
 
 interface GifService {
 
+    /**
+     * This function is used to query trending GIFs.
+     *
+     * @param limit the number of GIFs to request from the API
+     * @param offset the number of GIFs the API should skip before returning (used for pagination)
+     *
+     * @return response containing a list of GIFs
+     */
     @GET("v1/gifs/trending?api_key=${Secrets.API_KEY}&fmt=json")
     fun getTrending(@Query("limit") limit: Int, @Query("offset") offset: Int): Call<GifResponse>
 
+    /**
+     * This function is used to query GIFs matching a specific search string.
+     *
+     * @param q search string against which GIFs will be compared to find relevant results
+     * @param limit the number of GIFs to request from the API
+     * @param offset the number of GIFs the API should skip before returning (used for pagination)
+     *
+     * @return response containing a list of GIFs
+     */
     @GET("v1/gifs/search?api_key=${Secrets.API_KEY}&fmt=json")
     fun searchGifs(
         @Query("q") search: String,
@@ -20,6 +37,9 @@ interface GifService {
     ): Call<GifResponse>
 }
 
+/**
+ * This class is necessary to parse the list of GIFs out of the network responses.
+ */
 data class GifResponse(
     @Json(name = "data") val gifs: List<Gif>
 )
