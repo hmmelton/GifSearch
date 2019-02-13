5c1b8e713de6a7647b5c4c3d29d134f624c31b2a
Harrison Melton <harrisonmelton@Harrisons-MacBook-Pro.local>
2019-02-12 23:10:02 -0800

    refactored MainActivity into activities package

--- app/src/main/java/com/hmmelton/gifsearch/activities/MainActivity.kt
+++ app/src/main/java/com/hmmelton/gifsearch/activities/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.hmmelton.gifsearch
+package com.hmmelton.gifsearch.activities
 
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import com.hmmelton.gifsearch.R
 
 import kotlinx.android.synthetic.main.activity_main.*
 
